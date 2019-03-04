0f78e1ed821fe7696f8198e6517d6dd5ff7b86e0
Geerocktricks <geerockface4@gmail.com>
2019-03-04 22:24:50 +0300

    This commit creates the Category model

--- galleria/models.py
+++ galleria/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length = 30)
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
