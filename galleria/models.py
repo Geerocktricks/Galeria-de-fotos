4e1ddd415e32b33e53c72bd6c2fdf244caa0f0d1
Geerocktricks <geerockface4@gmail.com>
2019-03-04 23:01:20 +0300

    This commit creates the super user(admin)

--- galleria/models.py
+++ galleria/models.py
@@ -4,12 +4,22 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length = 30)
 
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length = 30)
 
-class Image(models.Model):
-    image_url = models.ImageField(upload_to = "images/")
-    name = models.CharField(max_length = 30)
-    description = models.TextField()
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    def __str__(self):
+        return self.name
+
+# class Image(models.Model):
+#     image_url = models.ImageField(upload_to = "images/")
+#     name = models.CharField(max_length = 30)
+#     description = models.TextField()
+#     location = models.ForeignKey(Location)
+#     category = models.ForeignKey(Category)
+    #   pub_date = models.DateTimeField(auto_now_add=True)
+
+    #    def __str__(self):
+    #            return self.name
