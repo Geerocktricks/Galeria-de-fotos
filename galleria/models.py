59be6db066649aaa8c688b2130a3050913a67e89
Geerocktricks <geerockface4@gmail.com>
2019-03-04 22:25:55 +0300

    This commit creates the Image model

--- galleria/models.py
+++ galleria/models.py
@@ -6,3 +6,10 @@ class Location(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length = 30)
+
+class Image(models.Model):
+    image_url = models.ImageField(upload_to = "images/")
+    name = models.CharField(max_length = 30)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
