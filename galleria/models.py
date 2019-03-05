927a91e3d7a8a2331fcf9470967bfa00ddbd42f2
Geerocktricks <geerockface4@gmail.com>
2019-03-05 05:28:43 +0300

    This commit cretates the search form

--- galleria/models.py
+++ galleria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Location(models.Model):
@@ -13,13 +14,46 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-# class Image(models.Model):
-#     image_url = models.ImageField(upload_to = "images/")
-#     name = models.CharField(max_length = 30)
-#     description = models.TextField()
-#     location = models.ForeignKey(Location)
-#     category = models.ForeignKey(Category)
-    #   pub_date = models.DateTimeField(auto_now_add=True)
+class Image(models.Model):
+    image_url = models.ImageField(upload_to = "images/")
+    name = models.CharField(max_length = 30)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
-    #    def __str__(self):
-    #            return self.name
+    def __str__(self):
+            return self.name
+
+
+    @classmethod
+    def get_image_by_id(cls,image_id):
+        """
+        This is the method to get a specific image
+        """
+        return cls.objects.get(id = image_id)
+
+    @classmethod
+    def get_images(cls):
+        return cls.objects.all()
+
+    @classmethod
+    def search_image(cls,category):
+        """
+        This is the method to search images based on a specific category
+        """
+        try:   
+            searched_category = Category.objects.filter(name__icontains  = category)[0]
+            return cls.objects.filter(category_id = searched_category.id)
+        except Exception:
+            return "No images found"
+    @classmethod
+    def filter_by_location(cls,location):
+        """
+        This is the method to get images taken in a certain location
+        """
+        the_location = Location.objects.get(name = location)
+        return cls.objects.filter(location_id = the_location.id)
+
+    def __str__(self):
+        return self.name
--- galleria/urls.py
+++ galleria/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('^today/$',views.day_today,name='Today'),
     url(r'^categories/(\d{4}-\d{2}-\d{2})/$',views.photo_category,name = 'category')
+    url(r'^search/', views.search_results, name='search_results')
 ]
